5c02bcdfae4b8adef0a8272727788eaa6badfc2b
dsomel21 <dsomel21@gmail.com>
2016-09-15 14:18:50 -0400

    nice parallax effect

--- script.js
+++ script.js
@@ -1,57 +1,10 @@
-console.log("WAHEGURU");
+/* Parallax Scrolling Feature */
+var yPos;
+var backgroundImage;
 
-
-function scroll(scrollY, headerSize) {
-  console.log(scrollY);
-  console.log(headerSize);
-
-  if(scrollY >= headerSize) {
-    $('header').css({
-      'top' : '0px'
-    });
-  }
-  else
-  {
-    $('header').css({
-      'top' : '-' + headerSize + 'px'
-    });
-  }
-}
-
-$(document).ready(function() {
-  $(window).load(function(){
-    var windowHeight        = $(window).height(),
-    headerSize        = $('header').height(),
-    heightDocument      = (windowHeight) + ($('.content').height()) - 20;
-
-    $('#scroll, #scroll-main').css({
-      'height' :  heightDocument + 'px'
-    });
-
-    $('header').css({
-      'height' : windowHeight + 'px',
-      'line-height' : windowHeight + 'px'
-    });
-
-    $('.wrapper').css({
-      'margin-top' : windowHeight + 'px'
-    });
-  });
-});
-  // scrollFooter(window.scrollY, footerHeight);
-
-  //   // ao dar rolagem
-  //   window.onscroll = function(){
-  //     var scroll = window.scrollY;
-
-  //     $('#scroll-animate-main').css({
-  //       'top' : '-' + scroll + 'px'
-  //     });
-
-  //     $('header').css({
-  //       'background-position-y' : 50 - (scroll * 100 / heightDocument) + '%'
-  //     });
-
-  //     scrollFooter(scroll, footerHeight);
-  //   }
-  // });
+window.addEventListener('scroll', function(){
+	debugger;
+	yPos = window.pageYOffset;
+	backgroundImage = document.getElementById('image');
+	backgroundImage.style.top = yPos * 1 + "px";
+})
