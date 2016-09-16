67e7387c9c61660b2c7dd5a87e542b12c559500a
dsomel21 <dsomel21@gmail.com>
2016-09-15 22:52:47 -0400

    adding cool scroll animations, but FOR some reason the responsiveness just completely stopped working...

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ window.addEventListener('scroll', function(){
 	debugger;
 	yPos = window.pageYOffset;
 	backgroundImage = document.getElementById('image');
-	backgroundImage.style.top = yPos * 1 + "px";
+	backgroundImage.style.top = yPos * .4 + "px";
 })
