19cffe6ee3c925a8f0b9ea850c6f492d9b818e40
dsomel21 <dsomel21@gmail.com>
2016-09-19 15:04:26 -0400

    having trouble with the sizing

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var yPos;
 var backgroundImage;
 
 window.addEventListener('scroll', function(){
-	debugger;
 	yPos = window.pageYOffset;
 	backgroundImage = document.getElementById('image');
 	backgroundImage.style.top = yPos * .4 + "px";
